9fe500213caf0f0887eb2bbd6dcf77cad4791b78
taha mir <tahamir999@gmail.com>
2021-07-07 23:49:35 +0500

    fetch api call add krdi hai url change krlena

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import { useState } from 'react';
 import './contactform.css'
 function ContactFrom() {
+
+    const [form,setForm]= useState({
+        fullname:'',
+        email:'',
+        message:'',
+        mailto:'tahamir999@gmail.com',
+        subject:'Mail from Go Green Pakistan Website'
+
+    })
+
     function submitMail(e) {
           e.preventDefault();
 
+          fetch('https://mywebsite.com/endpoint/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form)
+                     })
+
+
+
+                     
+
+    }
+    function handleInput(event){
+        setForm((oldvals)=>{
+            return {...oldvals,[event.target.name]:event.target.value}
+        })
+        
     }
     return (
         <div className="cform">
@@ -12,13 +42,13 @@ function ContactFrom() {
             <div className="form">
                 <form onSubmit = {submitMail}>
                     <label for="name" className="labels">Your name</label>
-                    <input id="name" type="text" name="fullname" className="inputfield" placeholder="Enter your name"/>
+                    <input id="name" type="text" name="fullname" className="inputfield" placeholder="Enter your name" onChange={handleInput}/>
                     
                     <label for="email" className="labels">Email address</label>
-                    <input id="email" type="email" name="email" className="inputfield" placeholder="Enter your email"/>
+                    <input id="email" type="email" name="email" className="inputfield" placeholder="Enter your email" onChange={handleInput}/>
                    
                     <label for="message" className="labels">Message</label>
-                    <textarea id="message" className="inputfield" id="tarea" style={{resize: 'none'}} placeholder="Enter your message"></textarea>
+                    <textarea name="message" id="message" className="inputfield" id="tarea" style={{resize: 'none'}} placeholder="Enter your message" onChange={handleInput}></textarea>
                     <input type="submit" value="Send" className="submitbtn"/>
                 </form>
             </div>
